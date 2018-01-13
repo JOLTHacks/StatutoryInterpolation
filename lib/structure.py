003a3d03733f5276f0bd43866e5cb91b1bcb3535
LeRoi <kxhere@gmail.com>
2018-01-13 14:45:53 -0500

    Add title structure and diff, representation enums
    Also add description to server.py.
    Add recursive definition for title structure display.
    Add enums for representations and diff types.

---
+++ lib/structure.py
@@ -0,0 +1,27 @@
+### Data structures
+from enum import Enum
+
+class Representation(Enum):
+    NUMERIC = 0
+    ROMAN = 1
+    SMALL_ROMAN = 2
+    UPPERCASE = 3
+    LOWERCASE = 4
+
+class DiffType(Enum):
+    ADD = 0
+    REMOVE = 1
+    UPDATE = 2
+
+class Structure():
+    """A path to an arbitrary portion of the U.S. Code.
+    Recursive data structure."""
+    def __init__(self, section, representation, text=None, subsections=[]):
+        self.section = section
+        self.representation = representation
+        self.text = text
+        self.subsections = subsections
+
+    def has_children():
+        return len(self.subsections) > 0
+
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+### REST API server which hosts version history for the U.S. Code
 import flask, os
 from flask import jsonify, Flask
 
@@ -16,6 +17,8 @@ def getDiffs():
     return jsonify([18])
 
 if __name__ == '__main__':
+    # Startup file load goes here.
+    # Probably add some logging too.
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
