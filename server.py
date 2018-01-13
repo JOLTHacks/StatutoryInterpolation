6c8b3294739280e46e7b03e881bd48f62d9fdec2
LeRoi <kxhere@gmail.com>
2018-01-13 14:29:02 -0500

    Add bare bones flask server
    Server has hardcoded getTitles method, and stands up when run.

---
+++ server.py
@@ -0,0 +1,13 @@
+import flask, os
+from flask import jsonify, Flask
+
+app = Flask(__name__)
+
+@app.route('getTitles')
+def getTitles():
+    return jsonify([18])
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
