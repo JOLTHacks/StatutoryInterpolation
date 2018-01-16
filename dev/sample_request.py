0b088922f80bc39bb3be167b442fefabb154c0a7
LeRoi <kxhere@gmail.com>
2018-01-15 20:20:34 -0500

    Add selection to sample_request.py

--- dev/sample_request.py
+++ dev/sample_request.py
@@ -1,5 +1,34 @@
 import requests, urllib2
 
-r = requests.request(method='get', url='http://127.0.0.1:5000/getTitle',\
-                 data='{"title": 10}')
-print r, r.text
+def getTitles():
+    r = requests.request(method='get', url='http://127.0.0.1:5000/getTitles')
+    print r, r.text
+
+def getTitle():
+    r = requests.request(method='get', url='http://127.0.0.1:5000/getTitle',\
+                     data='{"title": 10}')
+    print r, r.text
+
+methods = ["getTitles", "getTitle"]
+method_name_to_method = {"getTitles" : getTitles,
+                         "getTitle" : getTitle}
+
+def print_methods():
+    for m in range(len(methods)):
+        print "%d: %s" % (m, methods[m])
+
+if __name__ == "__main__":
+    while True:
+        print_methods()
+        action = raw_input("Selection: ")
+        valid = True
+        try:
+            action = int(action)
+        except:
+            valid = False
+            
+        if valid and action < len(methods):
+            method_name_to_method[methods[action]]()
+        else:
+            print "Invalid selection"
+        
